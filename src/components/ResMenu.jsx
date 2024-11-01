a5cde349e20f1972fc661b9f31a5f8453e324adb
vasusharrma <sharmavashu099@gmail.com>
2024-11-01 22:29:26 +0530

    updated to get all resMenu items dynamically

--- src/components/ResMenu.jsx
+++ src/components/ResMenu.jsx
@@ -2,19 +2,20 @@ import {useState , useEffect} from 'react';
 import FakeUI from "./FakeUI.jsx";
 import ResMenuItems from "./ResMenuItems";
 import ResMenuHandler from "./ResMenuHandler";
+import {useParams} from "react-router-dom";
 
 
 const ResMenu = () => {
     const[resInfo , setResInfo] = useState(null);
     const[initialResInfo , setInitialResInfo] = useState([]);
-
+    const{resId} = useParams();
 
     useEffect(() => {
         fetched();
     } , []);
 
     const fetched = async () => {
-        const res = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.9680035&lng=77.55520659999999&restaurantId=518651&catalog_qa=undefined&submitAction=ENTER\n");
+        const res = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=29.9680035&lng=77.55520659999999&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER\n`);
         const toJson = await res.json();
         setResInfo(toJson);
         console.log("res info is " , toJson);
@@ -38,19 +39,20 @@ const ResMenu = () => {
     const card9 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[9]?.card?.card;
     const card10 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[10]?.card?.card;
     const card11 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[11]?.card?.card;
-    // const card12 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[12]?.card?.card;
-    // const card13 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[13]?.card?.card;
-    // const card14 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[14]?.card?.card;
-    // const card15 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[15]?.card?.card;
+    const card12 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[12]?.card?.card;
+    const card13 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[13]?.card?.card;
+    const card14 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[14]?.card?.card;
+    const card15 = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[15]?.card?.card;
 
+    console.log("itemCards is " , )
 
-
-    const cardArr = [
-        cardRecom , card99 , card139 , card169 , cardCMD , cardCST , card8, card9, card10 , card11,
+    const tempCardArr = [
+        cardRecom , card99 , card139 , card169 , cardCMD , cardCST , card8, card9, card10 , card11,card12 , card13 , card14 , card15
     ];
 
-    const newArr = cardArr.map((i) => i);
-    console.log("arr is the " , newArr);
+    const cardArr = tempCardArr.filter((i) => {
+        return (i?.itemCards != undefined && i?.itemCards != null)
+    })
 
     return(
         <div className="resmenu_out_outside">
@@ -66,8 +68,7 @@ const ResMenu = () => {
                     </div>
                 </div>
                 {
-                    // <ResMenuHandler itemCards={cardRecom.itemCards} title={cardRecom.title} />
-                    cardArr.map((i) => <ResMenuHandler itemCards={i.itemCards}  title={i.title} />)
+                    cardArr.map((i) => <ResMenuHandler itemCards={i?.itemCards}  title={i?.title} />)
                 }
             </div>
         </div>
