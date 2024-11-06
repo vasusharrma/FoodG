911c00ea15dbc60507a5859905a389f3a3000598
vasusharrma <sharmavashu099@gmail.com>
2024-11-06 09:35:25 +0530

    changed about use

--- src/components/AboutUser.jsx
+++ src/components/AboutUser.jsx
@@ -1,41 +1,79 @@
 
-
 import {Component} from 'react';
 
 class AboutUser extends Component{
-    
+
     constructor(props){
         super(props);
 
         this.state = {
             userInfo:{
                 name:"Vasu",
-                    location:"Roorkee"
+                bio:"kabutar ja"
             },
+            sUserName:this.props.userName,
+            shouldFetch:false
         };
-        
-
     }
-    
 
     async componentDidMount(){
-        const{userName}  = this.props;
-        const fetchedUser = await fetch(`https://api.github.com/users/${userName}`);
-        const userToJson = await fetchedUser.json();
+        const {sUserName} = this.state;
+        const getData = await fetch(` https://api.github.com/users/${sUserName}`);
+        const dataToJson = await getData.json();
 
         this.setState({
-            userInfo:userToJson,
-        });
+            userInfo:dataToJson,
+            sUserName:"",
+        })
 
-        console.log(userToJson);
-
+            console.log("is fetched " , dataToJson);
 
     }
-    //
+
+
+   async componentDidUpdate(){
+
+
+        const {sUserName , shouldFetch} = this.state;
+        console.log("shouldfetch  " , shouldFetch);
+        if(shouldFetch === true){
+            const getData = await fetch(` https://api.github.com/users/${sUserName}`);
+            const dataToJson = await getData.json();
+            this.setState({
+                userInfo:dataToJson,
+                shouldFetch:false,
+                sUserName:"",
+            })
+        }
+
+   }
+
     render(){
 
         const {name , bio , location , avatar_url } = this.state.userInfo;
+        const{sUserName} = this.state;
+
+        console.log(this.state.userInfo);
         return(
+            <div className='user_about_outside'>
+            <div className='user_about_btn'>
+            <input type='text'  onChange = {
+                (e) => {
+                    this.setState(
+                        {
+                            sUserName:e.target.value,
+                        }
+                    )
+                }
+            } value={sUserName} />
+        <button className='resBtn' onClick={
+            () => {
+                this.setState({
+                    shouldFetch:true,
+                })
+            }
+        } >Search User</button>
+            </div>
            <div className="user_about">
                    <div className="user_img">
                        <img src={avatar_url}/>
@@ -43,11 +81,11 @@ class AboutUser extends Component{
                <h4 className='user_name'>{name}</h4>
                <p className='user_bio'>{bio}</p>
                <p className='user_location'>{location}</p>
-
+                </div>
             </div>
         )
     }
 
 }
-
 export default AboutUser;
+
